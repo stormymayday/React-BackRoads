a0e3c6389cb11e137fa35971a60e5797b285d93f
stormymayday <stormymayday7@gmail.com>
2023-08-21 15:57:13 -0700

    Displaying services dynamically by mapping over the array

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,6 +9,26 @@ const Services = () => {
 
             <div className="section-center services-center">
 
+                {
+                    services.map((service) => {
+
+                        const { id, icon, title, text } = service;
+
+                        return (
+
+                            <article className="service">
+                                <span className="service-icon"><i className={icon}></i></span>
+                                <div className="service-info">
+                                    <h4 className="service-title">{title}</h4>
+                                    <p className="service-text">{text}</p>
+                                </div>
+                            </article>
+
+                        );
+
+                    })
+                }
+
                 <article className="service">
                     <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
                     <div className="service-info">
