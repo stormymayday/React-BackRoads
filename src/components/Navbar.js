f512ff6cac939ea7c6cb7adac4f7120ba365e414
stormymayday <stormymayday7@gmail.com>
2023-08-21 15:05:09 -0700

    Removed the hardcoded Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,18 @@ import { pageLinks } from '../data.js';
 
 const Navbar = () => {
     return (
+
         <nav className="navbar">
+
             <div className="nav-center">
+
                 <div className="nav-header">
                     <img src={logo} className="nav-logo" alt="backroads" />
                     <button type="button" className="nav-toggle" id="nav-toggle">
                         <i className="fas fa-bars"></i>
                     </button>
                 </div>
+
                 <ul className="nav-links" id="nav-links">
 
                     {
@@ -29,21 +33,6 @@ const Navbar = () => {
                         })
                     }
 
-                    <li>
-                        <a href="#home" className="nav-link"> home </a>
-                    </li>
-
-                    <li>
-                        <a href="#about" className="nav-link"> about </a>
-                    </li>
-
-                    <li>
-                        <a href="#services" className="nav-link"> services </a>
-                    </li>
-
-                    <li>
-                        <a href="#tours" className="nav-link"> tours</a>
-                    </li>
                 </ul>
 
                 <ul className="nav-icons">
