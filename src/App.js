17c94ff8a8fc942f1fa79c63087ab54d0ad031b3
stormymayday <stormymayday7@gmail.com>
2023-08-20 20:32:55 -0700

    Created Hero component

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
 
       <Navbar />
 
-      <section className="hero" id="home">
-        <div className="hero-banner">
-          <h1>continue exploring</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            explicabo debitis est autem dicta.
-          </p>
-          <a href="#tours" className="btn hero-btn">explore tours</a>
-        </div>
-      </section>
-
       <section className="section" id="about">
         <div className="section-title">
           <h2>about <span>us</span></h2>
