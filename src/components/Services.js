4177f8a375cb8fc6227e2478e47b0152dcd57f14
stormymayday <stormymayday7@gmail.com>
2023-08-21 15:57:45 -0700

    Removed the hardcoded services articles

--- src/components/Services.js
+++ src/components/Services.js
@@ -29,39 +29,8 @@ const Services = () => {
                     })
                 }
 
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-wallet fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">saving money</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-tree fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">endless hiking</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
-
-                <article className="service">
-                    <span className="service-icon"><i className="fas fa-socks fa-fw"></i></span>
-                    <div className="service-info">
-                        <h4 className="service-title">amazing comfort</h4>
-                        <p className="service-text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Asperiores, officia.
-                        </p>
-                    </div>
-                </article>
             </div>
+
         </section>
     );
 };
