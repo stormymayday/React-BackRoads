41e54ea8381313b57ca91e84a3ed5a70cae62b3a
stormymayday <stormymayday7@gmail.com>
2023-08-21 15:23:09 -0700

    Removed the hardcoded social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,21 +55,6 @@ const Navbar = () => {
                         })
                     }
 
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                        ><i className="fab fa-facebook"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                        ><i className="fab fa-twitter"></i
-                        ></a>
-                    </li>
-                    <li>
-                        <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-                        ><i className="fab fa-squarespace"></i
-                        ></a>
-                    </li>
                 </ul>
             </div>
         </nav >
