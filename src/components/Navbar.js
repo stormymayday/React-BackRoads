0c34705d357f0d5d31d355f62c6af0509d1b5bf7
stormymayday <stormymayday7@gmail.com>
2023-08-21 15:22:38 -0700

    Displaying socialLinks dynamically by mapping over the array

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,6 +36,25 @@ const Navbar = () => {
                 </ul>
 
                 <ul className="nav-icons">
+
+                    {
+                        socialLinks.map((link) => {
+
+                            const { id, href, icon } = link;
+
+                            return (
+
+                                <li key={id}>
+                                    <a href={href} target="_blank" className="nav-icon"
+                                    ><i className={icon}></i
+                                    ></a>
+                                </li>
+
+                            );
+
+                        })
+                    }
+
                     <li>
                         <a href="https://www.twitter.com" target="_blank" className="nav-icon"
                         ><i className="fab fa-facebook"></i
