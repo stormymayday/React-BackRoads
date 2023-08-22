d18c3679998e2b1f0b9b83795baf7b4a21fea12a
stormymayday <stormymayday7@gmail.com>
2023-08-21 19:33:17 -0700

    Rendering Service instead of the article

--- src/components/Services.js
+++ src/components/Services.js
@@ -13,17 +13,9 @@ const Services = () => {
                 {
                     services.map((service) => {
 
-                        const { id, icon, title, text } = service;
-
                         return (
 
-                            <article key={id} className="service">
-                                <span className="service-icon"><i className={icon}></i></span>
-                                <div className="service-info">
-                                    <h4 className="service-title">{title}</h4>
-                                    <p className="service-text">{text}</p>
-                                </div>
-                            </article>
+                            <Service {...service} />
 
                         );
 
