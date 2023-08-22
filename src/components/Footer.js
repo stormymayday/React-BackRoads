2f8436d3b445238a17f084abae03c644485b9b3f
stormymayday <stormymayday7@gmail.com>
2023-08-21 18:45:42 -0700

    Dynamically displaying the footer-links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,23 @@ const Footer = () => {
     return (
         <footer className="section footer">
             <ul className="footer-links">
+
+                {
+                    pageLinks.map((link) => {
+
+                        const { id, href, text } = link;
+
+                        return (
+
+                            <li key={id}>
+                                <a href={href} className="footer-link">{text}</a>
+                            </li>
+
+                        );
+
+                    })
+                }
+
                 <li>
                     <a href="#home" className="footer-link">home</a>
                 </li>
